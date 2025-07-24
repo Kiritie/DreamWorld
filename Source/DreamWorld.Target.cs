01e4ac4f2d6b338a804cc995cd0d0fec32a3c0a7
kirito <810238014@qq.com>
2025-07-24 18:49:05 +0800

    优化角色动画

--- Source/DreamWorld.Target.cs
+++ Source/DreamWorld.Target.cs
@@ -9,7 +9,7 @@ public class DreamWorldTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V4;	
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("DreamWorld");
 	}
 }
